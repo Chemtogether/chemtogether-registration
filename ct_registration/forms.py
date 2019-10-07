e17378eaf77fbc1404e10ff59ef5b7405d87e882
Andreas Gimpel <an.gimpel@gmx.de>
2019-10-07 23:54:43 +0200

    change description of email field

--- ct_registration/forms.py
+++ ct_registration/forms.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 class SignupForm(UserCreationForm):
 
-    email = forms.EmailField(max_length=200, help_text='Required.')
+    email = forms.EmailField(max_length=200, help_text='Required. This email will only be used for verification.')
 
     class Meta:
         model = User
